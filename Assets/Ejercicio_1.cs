5585c690da78715f2cbdfa6f1eb33d6efec14604
dylanfuks18 <dylanfuks18@gmail.com>
2022-04-11 12:05:11 -0300

    Ejercicio prueba tema 1

--- Assets/Ejercicio_1.cs
+++ Assets/Ejercicio_1.cs
@@ -4,10 +4,36 @@ using UnityEngine;
 
 public class Ejercicio_1 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int moneda_extranjera;
+   
+    public int cantpesos_argentinos;
+     
+
     void Start()
     {
-        
+         int D = 112;
+         int R = 23;
+         int E = 122;
+    int dinerotraspaso;
+        dinerotraspaso = (cantpesos_argentinos / 112|23|122);
+        Debug.Log("El traspaso de pesos argentinos a su moneda es de: " + dinerotraspaso);
+
+        if  (moneda_extranjera != D | moneda_extranjera!= R | moneda_extranjera != E)
+        {
+            Debug.Log(" Opción de moneda extranjera no válida”.");
+        }
+        else
+        {
+
+        } 
+
+        if (dinerotraspaso < 1000)
+        {
+            Debug.Log("El monto mínimo es $1000");
+        }
+        else
+            Debug.Log(+ cantpesos_argentinos + "dinero ingresado, equivalen a " + dinerotraspaso);
+
     }
 
     // Update is called once per frame
